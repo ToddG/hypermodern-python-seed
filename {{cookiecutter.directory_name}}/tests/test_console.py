153044b19bd826146bd70227ef7ddadcc75147b3
toddg <toddg@zwrob.com>
2020-11-09 18:12:21 -0800

    add basic console test

---
+++ {{cookiecutter.directory_name}}/tests/test_console.py
@@ -0,0 +1,14 @@
+import click.testing
+import pytest
+
+from {{cookiecutter.project_name}} import console
+
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeeds(runner):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
