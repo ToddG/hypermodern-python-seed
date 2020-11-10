6188d056968ede885668657cfaef0355b5bb1ae2
toddg <toddg@zwrob.com>
2020-11-09 21:17:07 -0800

    add black and flake8-black

--- {{cookiecutter.directory_name}}/noxfile.py
+++ {{cookiecutter.directory_name}}/noxfile.py
@@ -1,12 +1,13 @@
 import nox
 
 locations = "src", "tests", "noxfile.py"
+nox.options.sessions = "lint", "tests"
 
 
 @nox.session(python=["3.7", "3.8"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8")
+    session.install("flake8", "flake8-black")
     session.run("flake8", *args)
 
 
@@ -15,3 +16,10 @@ def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session(python=["3.7", "3.8"])
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
