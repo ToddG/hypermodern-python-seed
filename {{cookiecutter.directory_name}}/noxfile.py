de471b3f41cf508c9174fc98641fc2f06b73976a
toddg <toddg@zwrob.com>
2020-11-09 21:11:44 -0800

    add flake8

---
+++ {{cookiecutter.directory_name}}/noxfile.py
@@ -0,0 +1,17 @@
+import nox
+
+locations = "src", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.7", "3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.7", "3.8"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", external=True)
+    session.run("pytest", *args)
