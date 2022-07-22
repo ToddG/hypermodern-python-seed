b7dfceec210d9dcec01ce6ed1cc24e9fafbf4ec4
Todd Greenwood-Geer <toddg@envirosoftwaresolutions.com>
2022-07-22 14:18:02 -0700

    add id/gid to docker run commands

--- {{cookiecutter.directory_name}}/scripts/run-command.sh
+++ {{cookiecutter.directory_name}}/scripts/run-command.sh
@@ -3,5 +3,5 @@
 # run this from project root directory
 
 # run arbitrary command within docker container
-docker run -v `(pwd)`:/project -w /project -it {{cookiecutter.project_name}}-build-container:latest $*
+docker run -v `(pwd)`:/project -u `id -u`:`id -g` -w /project -it {{cookiecutter.project_name}}-build-container:latest $*
 
